2f8247c0b5619f2b51692fb223b1fbb55dcc14d7
dianamcnulty <dianamcnulty@gmail.com>
2017-12-06 14:39:40 -0500

    gallery layout done

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SitesService } from './../services/sites/sites.service';
 
 @Component({
   selector: 'app-gallery',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GalleryComponent implements OnInit {
 
-  constructor() { }
+  sites: Array<any> = []
+  constructor(private sitesService: SitesService) { }
+
 
   ngOnInit() {
+    this.sites = this.sitesService.getSites();
+    console.log('sites is', this.sites)
   }
-
 }
