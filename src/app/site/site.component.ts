001dc7e88e78467bc0415bb4f5625124e97e04c7
dianamcnulty <dianamcnulty@gmail.com>
2017-12-06 15:59:45 -0500

    need testing, but site object should now be available to the site component.

--- src/app/site/site.component.ts
+++ src/app/site/site.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute }   from '@angular/router';
+import { SitesService } from './../services/sites/sites.service';
 
 @Component({
   selector: 'app-site',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./site.component.css']
 })
 export class SiteComponent implements OnInit {
+siteIndex: number;
+currentSite: Array<any> = []
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private sitesService: SitesService) { }
 
   ngOnInit() {
+    this.route.params.forEach( param => this.siteIndex = param.id )
+    sites = this.sitesService.getSites();
+    // need to find current site object using the site id
+    sites.forEach(site => {
+      if (site.id === siteIndex) {
+        this.currentSite = site
+        return this.currentSite
+      }
+  })
+
   }
 
 }
