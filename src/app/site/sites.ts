f7f8e64260d27c30692d8e175347fbb66f73562d
dianamcnulty <dianamcnulty@gmail.com>
2018-01-11 10:56:15 -0500

    update auth message in descriptions

--- src/app/site/sites.ts
+++ src/app/site/sites.ts
@@ -1,7 +1,7 @@
 export const SITES = [{
     id: 0,
     title: "Traveler's Keep",
-    description: "Traveler's Keep is an app to help those with a wanderlust keep the memories of their adventures in organized. The user is presented with a map of places they've seen. When they click on the map they are navigated to a collection of photos and stories from that journey that they uploaded. Users have the option to add more photos and stories, and can create new vacations at any time.",
+    description: "Traveler's Keep is an app to help those with a wanderlust keep the memories of their adventures in organized. The user is presented with a map of places they've seen. When they click on the map they are navigated to a collection of photos and stories from that journey that they uploaded. Users have the option to add more photos and stories, and can create new vacations at any time. The site requires authentication(username/password). Users are only able to view their own photos after logging in. Feel free to create a test account and interact with the site!",
     involvement: 'Solo Concept Project: User interface, and API: designed and implemented by Diana.',
     siteURL: 'https://dianamcnulty.github.io/travelerskeep-client/',
     gitHubRepo: 'https://github.com/dianamcnulty/travelerskeep-client',
@@ -15,7 +15,7 @@ export const SITES = [{
     screenShot: 'assets/images/ScreenShot-jewelry.png'},
   {id: 2,
     title: 'Flight Log',
-    description: "This single page app is for tracking paragliding/hang gliding flight data. Pilots can enter details about their flights, and retrieve a list of previous flights. They may also edit and delete previous flights. Certain stats are also displayed for the user, such as the number of flights they've had, total number of hours flown, what their longest or farthest flights were, etc. The app requires authentication. Users are only allowed to view their own flights. Though this concept is intended for paragliding/hang gliding, it could easily be adapted for other activities; for example: a travel log, or run tracker.",
+    description: "This single page app is for tracking paragliding/hang gliding flight data. Pilots can enter details about their flights, and retrieve a list of previous flights. They may also edit and delete previous flights. Certain stats are also displayed for the user, such as the number of flights they've had, total number of hours flown, what their longest or farthest flights were, etc. The app requires authentication. Users are only allowed to view their own flights. Feel free to create a test username/password at take it for a spin! Though this concept is intended for paragliding/hang gliding, it could easily be adapted for other activities; for example: a travel log, or run tracker.",
     involvement: 'Solo Concept Project- User interface, and API: designed and implemented by Diana',
     siteURL: 'https://dianamcnulty.github.io/flightlogclient/',
     gitHubRepo: 'https://github.com/dianamcnulty/flightlogclient',
